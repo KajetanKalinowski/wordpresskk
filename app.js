6c7b2941b72c4972866306ebc9a5dfc0a45ec9d7
KajetanKalinowski <kajtul999@gmail.com>
2023-10-06 14:54:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,24 +2,38 @@ async function getData(){
 const data = await fetch("http://localhost/kkwordpress/wp-json/wp/v2/posts")
 const json = await data.json()
 console.log(json)
+document.getElementById("list").innerHTML = " "
 for(let i=0;i<=json.length-1;i++){
     const list = document.getElementById("list")
     const li = document.createElement("li")
     const button = document.createElement("button")
     button.innerHTML="DELETE"
+    const buttoncens = document.createElement("button")
+    buttoncens.innerHTML = "Cenzura"
+    buttoncens.addEventListener("click",()=>{
+        cens(json[i].id)
+    })
     button.addEventListener("click",()=>{
         DELETE(json[i].id)
     })
     li.innerHTML= json[i].title.rendered
+    li.appendChild(buttoncens)
     li.appendChild(button)
     list.appendChild(li)
 }
 }
 getData()
 async function DELETE(id) {
-    wee
+    const data = await fetch(`http://localhost/kkwordpress/wp-json/wp/v2/posts/${id}`, {
+        method: "DELETE",
+        headers:{
+            Authorization:`Basic ${btoa("Kajetan:kajtulasty12")}`
+        }
+    })
+    const json = await data.json()
+    console.log(json)
 
-
+getData()
 }
 async function add(){
     const nazwa = document.getElementById("nazwa").value
@@ -42,16 +56,20 @@ async function add(){
     })
     // const data = await fetch(`http://localhost/kkwordpress/wp-json/wp/v2/posts?title=${nazwa}&status=publish&content=${tresc}`,)
     const json = await data.json()
-    const list = document.getElementById("list")
-    const li = document.createElement("li")
-    li.innerHTML= json.title.rendered
-    const button = document.createElement("button")
-    button.innerHTML="DELETE"
-    button.addEventListener("click",()=>{
-        DELETE(json.id)
+    
+    console.log(json)
+getData()
+}
+
+async function cens(id) {
+    const data = await fetch(`http://localhost/kkwordpress/wp-json/wp/v2/posts/${id}?status=publish&content=cenzura}`, {
+        method: "POST",
+        headers:{
+            Authorization:`Basic ${btoa("Kajetan:kajtulasty12")}`
+        }
     })
-    li.appendChild(button)
-    list.appendChild(li)
+    const json = await data.json()
     console.log(json)
 
+getData()
 }
